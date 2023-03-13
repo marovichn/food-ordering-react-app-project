7bb13bf3f3e8efc4e1fdefb00d2793e2895899b7
marovichn <nikolamarovic.11@gmail.com>
2023-03-13 19:02:54 +0100

    adding-demo-header

---
+++ FoodOrderingApp/src/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./Header.module.css";
+import meals from "../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>LoveMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="table of delicious food" />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;
