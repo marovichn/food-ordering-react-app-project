16f545711b415d0ac8950854f76296e7b65f754f
marovichn <nikolamarovic.11@gmail.com>
2023-03-14 22:04:12 +0100

    displaying-dummy-meals-demo

--- FoodOrderingApp/src/App.js
+++ FoodOrderingApp/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./Layout/Header";
+import Meals from "./Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header></Header>
       {/* <Cart></Cart> */}
-      {/* <Meals></Meals> */}
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
