5ee2482d344458d0d3a13a5faa16989c9290106e
marovichn <nikolamarovic.11@gmail.com>
2023-03-15 22:56:34 +0100

    using-reducer-for-managing-state

--- FoodOrderingApp/src/context/CartProvider.js
+++ FoodOrderingApp/src/context/CartProvider.js
@@ -1,11 +1,34 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const CartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+    const newTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: newTotalAmount };
+  }
+  /* } else if (action.type === "REMOVE") {
+  } */
+  return {};
+};
+
 const CartProvider = (props) => {
-  const addItemToCarthandler = (item) => {};
-  const removeItemToCarthandler = (id) => {};
+  const [cartState, dispatchCart] = useReducer(CartReducer, defaultCartState);
+  const addItemToCarthandler = (item) => {
+    dispatchCart({ type: "ADD", item: item });
+  };
+  const removeItemToCarthandler = (id) => {
+    dispatchCart({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
+    items: cartState.items,
     totalAmount: 0,
     addItem: addItemToCarthandler,
     removeItem: removeItemToCarthandler,
