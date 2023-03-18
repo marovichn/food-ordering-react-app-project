43a0968623c9ab4a2ced2274c0614af550273720
marovichn <nikolamarovic.11@gmail.com>
2023-03-18 01:22:17 +0100

    added-animations

--- FoodOrderingApp/src/components/Layout/HeaderCartButton.js
+++ FoodOrderingApp/src/components/Layout/HeaderCartButton.js
@@ -7,6 +7,22 @@ import classes from "./HeaderCartButton.module.css";
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
   const { items } = cartCtx;
+  const [isAnimated, setAnimate] = useState(false);
+  const btnClasses = `${classes.button} ${isAnimated ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setAnimate(true);
+    const timer = setTimeout(() => {
+      setAnimate(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
